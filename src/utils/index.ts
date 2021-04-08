9c2f0dbacffcdc4f787b42dcbdc90e13c7a90bf2
leegeunhyeok <dev.ghlee@gmail.com>
2021-04-08 21:21:50 +0900

    feat(util): add toDataURL
    - File to data url converter

---
+++ src/utils/index.ts
@@ -0,0 +1,15 @@
+export const toDataURL = (file: File): Promise<string> => {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onload = () => {
+      const data = reader.result;
+      if (data === null || data instanceof ArrayBuffer) {
+        reject();
+      } else {
+        resolve(data);
+      }
+    };
+    reader.onerror = () => reject();
+    reader.readAsDataURL(file);
+  });
+};
